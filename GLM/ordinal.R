0c7e8480bac78af418d53b689b721c9a76cc4ffb
DragonflyStats <kobriendublin@gmail.com>
2018-02-25 12:23:28 +0000

    Update and rename regression/ordinal.R to GLM/ordinal.R

--- GLM/ordinal.R
+++ GLM/ordinal.R
@@ -270,9 +270,3 @@ head(lnewdat)
 ggplot(lnewdat, aes(x = gpa, y = Probability, colour = Level)) +
   geom_line() + facet_grid(pared ~ public, labeller="label_both")
  predicted probability plot for public versus private by gpa
-Things to consider
-Perfect prediction: Perfect prediction means that one value of a predictor variable is associated with only one value of the response variable. If this happens, Stata will usually issue a note at the top of the output and will drop the cases so that the model can run.
-Sample size: Both ordered logistic and ordered probit, using maximum likelihood estimates, require sufficient sample size. How big is big is a topic of some debate, but they almost always require more cases than OLS regression.
-Empty cells or small cells: You should check for empty or small cells by doing a crosstab between categorical predictors and the outcome variable. If a cell has very few cases, the model may become unstable or it might not run at all.
-Pseudo-R-squared: There is no exact analog of the R-squared found in OLS. There are many versions of pseudo-R-squares. Please see Long and Freese 2005 for more details and explanations of various pseudo-R-squares.
-Diagnostics: Doing diagnostics for non-linear models is difficult,
