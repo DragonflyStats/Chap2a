eb88400ad7358eff83abccf182bb51199fdd4bb9
DragonflyStats <kobriendublin@gmail.com>
2018-03-22 09:11:35 +0000

    Create MCP.R

---
+++ MCP.R
@@ -0,0 +1,13 @@
+
+
+library(magrittr)
+library(dplyr)
+library(MASS)
+library(broom)
+library(modelr)
+
+####### Data Sets ####
+
+Cheeses <- read.csv("https://raw.githubusercontent.com/RWorkshop/workshopdatasets/master/Cheeses.csv")
+
+
